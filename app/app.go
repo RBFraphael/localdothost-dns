262aaeb3191ed932bbaff72bcccccc7c06f0e4e5
RBFraphael <rbfraphael@gmail.com>
2024-03-04 03:49:11 -0300

    Added IPv6

--- app/app.go
+++ app/app.go
@@ -25,8 +25,13 @@ func Init() *cli.App {
 			Usage:   "Change DNS settings",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name:  "ip",
+					Name:  "ipv4",
 					Value: "127.0.0.1",
+					Usage: "IP address to set as DNS server",
+				},
+				cli.StringFlag{
+					Name:  "ipv6",
+					Value: "::1",
 					Usage: "IP address to set as DNS server",
 				},
 			},
@@ -50,12 +55,13 @@ func Init() *cli.App {
 }
 
 func changeDns(c *cli.Context) {
-	if(!checkAdmin()) {
-		runMeElevated();
-		return;
+	if !checkAdmin() {
+		runMeElevated()
+		return
 	}
 
-	ip := c.String("ip")
+	ipv4 := c.String("ipv4")
+	ipv6 := c.String("ipv6")
 
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -64,8 +70,8 @@ func changeDns(c *cli.Context) {
 	}
 
 	for _, i := range interfaces {
-		fmt.Println("Setting DNS for", i.Name)
-		command := exec.Command("netsh", "interface", "ipv4", "set", "dns", "name="+i.Name, "static", ip);
+		fmt.Println("Setting IPv4 DNS for " + i.Name + " to " + ipv4)
+		command := exec.Command("netsh", "interface", "ipv4", "set", "dns", "name="+i.Name, "static", ipv4)
 		var out bytes.Buffer
 
 		command.Stdout = &out
@@ -73,6 +79,18 @@ func changeDns(c *cli.Context) {
 
 		if cmdErr != nil {
 			fmt.Println(cmdErr, out.String())
+			return
+		}
+
+		fmt.Println("Setting IPv6 DNS for " + i.Name + " to " + ipv6)
+		command = exec.Command("netsh", "interface", "ipv6", "set", "dns", "name="+i.Name, "static", ipv6)
+
+		command.Stdout = &out
+		cmdErr = command.Run()
+
+		if cmdErr != nil {
+			fmt.Println(cmdErr, out.String())
+			return
 		}
 	}
 }
@@ -90,9 +108,9 @@ func getInterfaces(c *cli.Context) {
 }
 
 func resetDNS(c *cli.Context) {
-	if(!checkAdmin()) {
-		runMeElevated();
-		return;
+	if !checkAdmin() {
+		runMeElevated()
+		return
 	}
 
 	interfaces, err := net.Interfaces()
@@ -103,7 +121,7 @@ func resetDNS(c *cli.Context) {
 
 	for _, i := range interfaces {
 		fmt.Println("Resetting DNS for", i.Name)
-		command := exec.Command("netsh", "interface", "ipv4", "set", "dns", "name="+i.Name, "dhcp");
+		command := exec.Command("netsh", "interface", "ipv4", "set", "dns", "name="+i.Name, "dhcp")
 		var out bytes.Buffer
 
 		command.Stdout = &out
@@ -116,25 +134,25 @@ func resetDNS(c *cli.Context) {
 }
 
 func runMeElevated() {
-    verb := "runas"
-    exe, _ := os.Executable()
-    cwd, _ := os.Getwd()
-    args := strings.Join(os.Args[1:], " ")
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
 
-    verbPtr, _ := syscall.UTF16PtrFromString(verb)
-    exePtr, _ := syscall.UTF16PtrFromString(exe)
-    cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
-    argPtr, _ := syscall.UTF16PtrFromString(args)
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
 
-    var showCmd int32 = 1 //SW_NORMAL
+	var showCmd int32 = 1 //SW_NORMAL
 
-    err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-    if err != nil {
-        fmt.Println(err)
-    }
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func checkAdmin() bool {
-    _, err := os.Open("\\\\.\\PHYSICALDRIVE0")
-    return err == nil
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+	return err == nil
 }
